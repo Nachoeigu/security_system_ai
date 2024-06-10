af59aeee7e799812aeb32b11c7958328eb166c0f
ignacio.eiguren@gmail.com <ignacio.eiguren@gmail.com>
2024-06-10 10:06:34 +0200

    .

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from model import Detector
+import time
 
+app = Detector(mode = 'openai')
 
-app = Detector(mode = 'google')
-
-print(app.analyzing_thief_detection())
+while True:
+    time.sleep(2)
+    result = app.analyzing_human_detection()
+    if result.is_human:
+        app.analyzing_thief_detection()
+    
